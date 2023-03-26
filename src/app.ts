d7eca0d03c356d9b3d00d1d123313ab125fa120d
Your Name Martin <you@example.comMartinMaffei95@gmail.com>
2023-03-26 00:07:39 -0300

    reconfig package.jso

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,7 @@ import dbConnect from './config/mongo';
 import cors from 'cors';
 
 //CREATE EXPRESS APP
-
+const PORT = process.env.PORT || 3001;
 const app = express();
 app.use(cors());
 
@@ -16,6 +16,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 //ROUTES WILL GO HERE
 app.use(router);
 
-dbConnect();
-
-app.listen(3000, () => console.log('Server started on port 3000'));
+dbConnect().then(() => {
+  console.log('Connection with DB is ready');
+});
+app.listen(PORT, () => {
+  console.log(`Server listen on port: ${PORT}`);
+});
