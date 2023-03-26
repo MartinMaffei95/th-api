3ba06e0e972abf2c793371b0de982d1d52fdc9bd
Martin Maffei <86947287+MartinMaffei95@users.noreply.github.com>
2023-03-26 09:28:25 -0300

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -15,10 +15,17 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 //ROUTES WILL GO HERE
 app.use(router);
-
+  const { DB_HOST, DB_USER, DB_PASS, DB_DATABASE } = process.env;
+  await connect(
+    // `mongodb://${DB_HOST}/${DB_DATABASE}`
 dbConnect().then(() => {
   console.log('Connection with DB is ready');
+  
 });
 app.listen(PORT, () => {
   console.log(`Server listen on port: ${PORT}`);
+    console.log(
+    `mongodb+srv://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_DATABASE}?retryWrites=true&w=majority`
+  
+  )
 });
