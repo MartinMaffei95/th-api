2af4de998287b8b9d2678c5b2db1469f12e8b8fb
Martin Maffei <86947287+MartinMaffei95@users.noreply.github.com>
2023-03-26 09:20:45 -0300

    Update mongo.ts

--- src/config/mongo.ts
+++ src/config/mongo.ts
@@ -3,6 +3,8 @@ import { connect } from 'mongoose';
 
 async function dbConnect(): Promise<void> {
   const { DB_HOST, DB_USER, DB_PASS, DB_DATABASE } = process.env;
+  console.log('BUSCANDO ERROR=>>', `mongodb+srv://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_DATABASE}?retryWrites=true&w=majority`
+)
   await connect(
     // `mongodb://${DB_HOST}/${DB_DATABASE}`
     `mongodb+srv://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_DATABASE}?retryWrites=true&w=majority`
